30ef1ef6f7befee0786893cc199864a076b47981
Yu MORIYA <euro21st@gmail.com>
2013-12-03 14:26:07 +0900

    add solution

---
+++ Telestrations/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Telestrations.MvcApplication" Language="C#" %>
---
+++ Telestrations/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Telestrations.Startup))]
+namespace Telestrations
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
